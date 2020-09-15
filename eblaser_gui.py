c94135f1997a27b66c4ece4b5b56b1653e8e8de2
Tiago Bortoletto Vaz <tiago@debian.org>
2020-09-15 17:05:53 -0400

    Adding local counter.

--- eblaser_gui.py
+++ eblaser_gui.py
@@ -5,12 +5,16 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 import time
 from datetime import datetime
+from datetime import timedelta
 import json
 import requests
 
 from gpiozero import Button, OutputDevice, InputDevice
 
+# Input from laser when it's in function
 laser_status = InputDevice(19)
+
+# Relay
 oe_pinout = OutputDevice(13)
 oe_pinout.on()
 
@@ -24,6 +28,9 @@ class CardReader(GridLayout):
     def __init__(self, **kwargs):
         super(CardReader, self).__init__(**kwargs)
         self.rfid = None
+        self.laser_time_start = 0
+        self.laser_time_end = 0
+        self.laser_time_total = 0
         self.laser_flag = True
         self.laser_flag_do_not_log = True
         self.p_laser_state = 0
@@ -34,32 +41,46 @@ class CardReader(GridLayout):
     def read_gpio(self, *args):
         # FIXME: try/exception + method for url request
         if self.rfid:
-            if laser_status.is_active is False: # opposite, fine
+            if laser_status.is_active is False: # opposite, fine => laser on
                 if self.laser_flag == False:
+                    # start counting time
+                    self.laser_time_start = time.time()
                     self.laser_flag = True
-                    self.laser_text.text = "Laser is on"
+                    self.laser_text.text = "Laser is ON"
                     url = "http://eblab.acaia.ca/rfid/?uid=" + self.rfid + "&resource=laser_cutter" + "&action=LAS"
                     r = requests.get(url)
-            elif laser_status.is_active is True:
+            # If laser becomes OFF and lastest ON was minimum 8s before, then turn it OFF for real
+            # otherwise it is just pulses from engraving activity
+            elif laser_status.is_active is True and int(timedelta(seconds=(time.time() - self.laser_time_start)).total_seconds()) > 7:
                 if self.laser_flag == True:
                     self.laser_flag = False
-                    self.laser_text.text = "Laser is off"
-                    url = "http://eblab.acaia.ca/rfid/?uid=" + self.rfid + "&resource=laser_cutter" + "&action=LAE"
                     if self.laser_flag_do_not_log == False: # do not log the first loop interaction
+                        ### laser time counter ###
+                        self.laser_time_end = time.time()
+                        self.laser_time_total = self.laser_time_total + (self.laser_time_end - self.laser_time_start)
+                        self.laser_time_start = 0
+                        self.laser_time_end = 0
+                        ##########################
+                        url = "http://eblab.acaia.ca/rfid/?uid=" + self.rfid + "&resource=laser_cutter" + "&action=LAE" + "&usage_time=" + str(round(self.laser_time_total))
                         r = requests.get(url)
+                        self.laser_text.text = "Laser is OFF"
                     self.laser_flag_do_not_log = False
 
     def start(self, *args):
         self.main_text = Label()
         self.main_text.font_size = "50px"
         self.add_widget(self.main_text)
-        
-        self.laser_text = Label(text="Laser is off")
+
+        self.laser_time_text = Label()
+        self.laser_time_text.font_size = "40px"
+        self.add_widget(self.laser_time_text)
+
+        self.laser_text = Label(text="Laser is OFF")
         self.laser_text.font_size = "25px"
         self.add_widget(self.laser_text)
 
-        self.gpio_event = Clock.schedule_interval(self.read_gpio, 0.1)
-        self.event = Clock.schedule_interval(self.update, 0.1)
+        self.gpio_event = Clock.schedule_interval(self.read_gpio, 0.05)
+        self.event = Clock.schedule_interval(self.update, 0.05)
 
     def load_json(self):
         with open('/tmp/eblaser_data.json') as json_file:
@@ -69,6 +90,10 @@ class CardReader(GridLayout):
         self.load_json()
         self.main_text.text = self.data['text']
         self.rfid = self.data['current_id']
+        # reset total laser time each time we have a logoff
+        if self.data['is_free'] == True:
+            self.laser_time_total = 0
+        self.laser_time_text.text = "Session usage: " + str(timedelta(seconds=round(self.laser_time_total)))
 
 class ScreenApp(GridLayout):
     def __init__(self, **kwargs):
@@ -97,3 +122,4 @@ if __name__ == '__main__':
     Config.set('graphics', 'window_state', 'maximized')
     Config.write()
     MyApp().run()
+
